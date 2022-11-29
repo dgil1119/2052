0ba1d207db146544f79d3804da05dd9423ab6b3c
tunick <dgil1119@ceuandalucia.es>
2022-11-29 20:51:57 +0100

    2051

---
+++ Array11/src/Array11/Array11.java
@@ -0,0 +1,13 @@
+package Array11;
+
+public class Array11 {
+ public static void main(String[] args) {
+	Integer[][] arraybidimensional=new Integer [10][5];
+	for(int i=0; i<arraybidimensional.length;i++) {
+		Integer ArrayDentro= arraybidimensional[];
+		for(int j=0; j<ArrayDentro; j++) {
+			ArrayDentro[j]=hv
+		}
+	}
+}
+}
